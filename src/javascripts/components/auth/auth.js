64655b30b7414eebc0891b833a38295475783f79
Jennifer Lipps <jjlipps@gmail.com>
2019-06-08 13:08:25 -0500

    authentication WIP

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import googleButton from './login.png';
+import util from '../../helpers/util';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const authStringBuilder = () => {
+  let domString = '<button id="google-auth" class="btn btn-warning">';
+  domString += `<img src=${googleButton}>`;
+  domString += '</button>';
+  util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
+};
+
+export default { authStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,14 @@
 import '../styles/main.scss';
 import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import auth from './components/auth/auth';
+import logout from './components/auth/logout';
 
-console.error('test');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.authStringBuilder();
+  logout.logoutEvent();
+};
+
+init();
