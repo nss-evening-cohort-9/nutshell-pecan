b207252fc8a8357f10bb4c58a3d80fd176ed3bf1
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-15 00:47:45 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,14 +5,14 @@ import apiKeys from './helpers/apiKeys.json';
 import auth from './components/auth/auth';
 import logout from './components/auth/logout';
 import authData from './helpers/data/authData';
-import events from './components/Events/events';
+// import events from './components/Events/events';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   logout.logoutEvent();
   authData.checkLoginStatus();
-  events.getEvents();
+  // events.getEvents();
 };
 
 init();
